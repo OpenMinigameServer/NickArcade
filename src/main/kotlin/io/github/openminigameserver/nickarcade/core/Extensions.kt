771875b2ee4acfb0e8777e261709ef2dac64ca73
NickAc <32451103+NickAcPT@users.noreply.github.com>
2021-03-12 01:36:17 +0000

    Add party chat separator as extension

--- src/main/kotlin/io/github/openminigameserver/nickarcade/core/Extensions.kt
+++ src/main/kotlin/io/github/openminigameserver/nickarcade/core/Extensions.kt
@@ -4,6 +4,14 @@ import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.toCollection
 import net.kyori.adventure.audience.Audience
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.newline
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.Style
+import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.litote.kmongo.property.KPropertyPath
@@ -46,3 +54,23 @@ val Double.ticks
 operator fun <T0, T1, T2, T3> KProperty1<T0, T1?>.div(p2: KProperty1<T2, T3?>): KProperty1<T0, T3?> =
     @Suppress("INVISIBLE_MEMBER")
     (KPropertyPath(this, p2))
+
+
+val separator = text(" ".repeat(64), Style.style(TextDecoration.STRIKETHROUGH))
+
+fun separator(color: TextColor = NamedTextColor.BLUE): Component {
+    return separator.style {
+        it.decorate(TextDecoration.STRIKETHROUGH)
+        it.color(color)
+    }
+}
+
+fun separator(color: TextColor = NamedTextColor.BLUE, builder: (TextComponent.Builder).() -> Unit): Component {
+    return text {
+        it.append(separator(color))
+        it.append(newline())
+        it.append(text(builder))
+        it.append(newline())
+        it.append(separator(color))
+    }
+}
