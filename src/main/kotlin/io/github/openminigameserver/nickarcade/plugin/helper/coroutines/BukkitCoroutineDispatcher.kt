8e9a86917062ff927caa15e11b392e69ec1ba201
NickAc <32451103+NickAcPT@users.noreply.github.com>
2021-03-12 01:40:10 +0000

    Handle Task scheduler errors by starting a new thread and running blocking there

--- src/main/kotlin/io/github/openminigameserver/nickarcade/plugin/helper/coroutines/BukkitCoroutineDispatcher.kt
+++ src/main/kotlin/io/github/openminigameserver/nickarcade/plugin/helper/coroutines/BukkitCoroutineDispatcher.kt
@@ -3,7 +3,9 @@ package io.github.openminigameserver.nickarcade.plugin.helper.coroutines
 import io.github.openminigameserver.nickarcade.core.IoC
 import io.github.openminigameserver.nickarcade.plugin.NickArcadePlugin
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.runBlocking
 import org.bukkit.Bukkit
+import kotlin.concurrent.thread
 import kotlin.coroutines.CoroutineContext
 
 object BukkitCoroutineDispatcher : CoroutineDispatcher() {
@@ -11,6 +13,10 @@ object BukkitCoroutineDispatcher : CoroutineDispatcher() {
      * Handles dispatching the coroutine on the correct thread.
      */
     override fun dispatch(context: CoroutineContext, block: Runnable) {
-        Bukkit.getScheduler().runTask(IoC<NickArcadePlugin>(), block)
+        try {
+            Bukkit.getScheduler().runTask(IoC<NickArcadePlugin>(), block)
+        } catch (e: Throwable) {
+            thread { runBlocking { block } }
+        }
     }
 }
